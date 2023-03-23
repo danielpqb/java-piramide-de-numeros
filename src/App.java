45b97373b4c6dc1b9e8cd6aa342b8c3de9731548
danielpqb <danielpqb@gmail.com>
2023-03-23 19:00:43 -0300

    complete

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Integer max = 5;
+        System.out.println("Pirâmide de números");
+
+        for (int i = 1; i <= max; i++) {
+            String myString = "";
+            for (int j = 1; j <= i; j++) {
+               myString = myString.concat(String.valueOf(i));
+            }
+            System.out.println(myString);
+        }
     }
 }
